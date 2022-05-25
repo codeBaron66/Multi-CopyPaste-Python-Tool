5bb7352a600a8e363eada0e9b45216cf9ae690cb
codeBaron66 <nickc87@hotmail.co.uk>
2022-05-25 19:07:36 +0100

    Code Finished

--- copypaste.py
+++ copypaste.py
@@ -1,3 +1,42 @@
 import sys
 import clipboard
 import json
+
+SAVED_DATA = "clipboard.json"
+
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+# save_items("test.json", {"key": "value"})
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+
+    if command == "save":
+        key = input("Enter Your Key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+        print("Data saved!")
+    elif command == "load":
+        key = input("Enter your key: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("Data copied to clipboard.")
+        else:
+            print("Key not found.")
+    elif command == "list":
+        print(data)
+    else:
+        print("Command Unknown")
+else:
+    print("Please type in one command: save, load or list")
